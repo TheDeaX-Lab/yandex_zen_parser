a3d0591999d2d1ef003b2f5474d38420892d15a5
TerryChan2003 <aleksandr_h2001@mail.ru>
2019-12-22 21:09:41 +0300

    update

--- ConsoleApp1/Person.cs
+++ ConsoleApp1/Person.cs
@@ -154,6 +154,142 @@ namespace ConsoleApp1
             public String text { get; set; }
         }
 
+        public class Auth
+        {
+            public Boolean is_authorized{ get; set; }
+        }
+
+        public class Exp
+        {
+            public String rezen_no_tablet{ get; set; }
+            public String desktop_rtb_ab{ get; set; }
+            public String tykva_enabled{ get; set; }
+            public String new_experiments_service{ get; set; }
+            [JsonPropertyName("default")]
+            public String defaulti { get; set; }
+        }
+
+        public class ExperimentsData
+        {
+            [JsonExtensionData] public IDictionary<string, JsonElement> extra { get; set; }
+        }
+
+        public class GridType
+        {
+            public String name{ get; set; }
+            public UInt16 column_count{ get; set; }
+            public Double portion_max_size{ get; set; }
+            public UInt16 portion_multiplicity{ get; set; }
+            public List<UInt16> small_positions{ get; set; }
+            public List<UInt16> big_positions{ get; set; }
+            public List<UInt16> grid_positions{ get; set; }
+        }
+
+        public class ClientLogs
+        {
+            public String link{ get; set; }
+        }
+
+        public class Channel
+        {
+            public class Source
+            {
+                public class LogoSizes
+                {
+                    public String framed_100x128_1x{ get; set; }
+                    public String framed_132x176_1x{ get; set; }
+                    public String framed_202x260_1x{ get; set; }
+                    public String framed_212x280_1x{ get; set; }
+                }
+
+                public String id{ get; set; }
+                public String strongest_id{ get; set; }
+                public String title{ get; set; }
+                public String status{ get; set; }
+                public String feed_link{ get; set; }
+                public String feed_share_link{ get; set; }
+                public String feed_api_link{ get; set; }
+                public String logo{ get; set; }
+                public String framed_logo{ get; set; }
+                public LogoSizes logo_sizes{ get; set; }
+                public String logo_background_color{ get; set; }
+                public String title_background_color{ get; set; }
+                public String title_color{ get; set; }
+                public String og_image{ get; set; }
+                public String subtitle{ get; set; }
+                public String description{ get; set; }
+                public UInt32 subscribers{ get; set; }
+                public List<JsonElement?> social_links{ get; set; }
+                public UInt32 audience{ get; set; }
+                public Boolean is_indexable{ get; set; }
+                public String type{ get; set; }
+                
+            }
+
+            public class StatEvents
+            {
+                public String feedback_favourite{ get; set; }
+                public String feedback_cancel_favourite{ get; set; }
+                public String feedback_block{ get; set; }
+                public String feedback_cancel_block{ get; set; }
+                public String complain_show{ get; set; }
+                public String complain_click{ get; set; }
+                public String share_click{ get; set; }
+                public String login_suggest_show{ get; set; }
+                public String login_suggest_close{ get; set; }
+                public String auth_click{ get; set; }
+                public String auth_login{ get; set; }
+            }
+
+            public String status{ get; set; }
+            public Source source{ get; set; }
+            public String og_image{ get; set; }
+            public Boolean is_indexable{ get; set; }
+            public StatEvents stat_events{ get; set; }
+            public String bulk_params{ get; set; }
+        }
+
+        public class More
+        {
+            public String link{ get; set; }
+        }
+
+        public class User
+        {
+            public class LastSawFeed
+            {
+                public JsonElement? zen_app{ get; set; }
+                public JsonElement? zen_app_ios{ get; set; }
+            }
+
+            public LastSawFeed last_saw_feed { get; set; }
+        }
+
+        public class Links
+        {
+            public String all_channels_link { get; set; }
+        }
+
         public List<Article> items { get; set; }
+        public String rid{ get; set; }
+        public Auth auth{ get; set; }
+        public UInt16 ttl{ get; set; }
+        public UInt32 store_ttl{ get; set; }
+        public Boolean have_zen{ get; set; }
+        public Boolean show_zen{ get; set; }
+        public Exp exp{ get; set; }
+        public ExperimentsData experiments_data{ get; set; }
+        public UInt64 generate_time{ get; set; }
+        public Boolean ice_start{ get; set; }
+        public GridType grid_type{ get; set; }
+        public ClientLogs client_logs{ get; set; }
+        public String platform_status{ get; set; }
+        public Channel channel{ get; set; }
+        public More more{ get; set; }
+        public List<JsonElement?> precache_resources{ get; set; }
+        public String user_status_on_publishers_platform{ get; set; }
+        public User user{ get; set; }
+        public Links links{ get; set; }
+        public String group_ids{ get; set; }
     }
 }
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,43 +1,56 @@
 ﻿#nullable enable
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Threading.Tasks;
 using System.Net.Http;
-using HtmlAgilityPack;
+using System.Text;
 using System.Text.Json;
+using HtmlAgilityPack;
 
 
 namespace ConsoleApp1
 {
-
     class Program
     {
         private static HtmlWeb loader = new HtmlWeb();
         private static HttpClient client = new HttpClient();
+        private static List<String> list_of_channels = new List<String>();
 
         private const String api_channel_id =
             "https://zen.yandex.ru/api/v3/launcher/export?_csrf=9831a76537b43259db7d8b0de06c7107291b4808-1572698342685&clid=300&country_code=ru&token=&channel_id={0}";
 
         private const String api_channel_name =
             "https://zen.yandex.ru/api/v3/launcher/export?_csrf=9831a76537b43259db7d8b0de06c7107291b4808-1572698342685&clid=300&country_code=ru&token=&channel_name={0}";
-        private static UInt16 pages = 25;
-        private static UInt16 pull_pages = 1;
-        private static UInt16 totalChannels = 0;
+
+        private static UInt16 pages = 100;
+        
+        private static UInt16 pull_pages = 10;
+        private static UInt16 pull_channels = 50;
+        
+        private static UInt16 _totalChannels = 0;
+        
+        private static UInt16 _completedPages = 0;
+        private static UInt16 _completedChannels = 0;
+        
+        private static UInt16 _currentPage = 1;
+        private static UInt16 _currentChannel = 1;
+        
+        private static UInt16 _pagesWorking = 0;
+        private static UInt16 _channelsWorking = 0;
+        
         private static DateTime start = DateTime.Now;
-        static async Task ParsePage(HtmlDocument doc)
+        static void ParsePage(HtmlDocument doc)
         {
-            var tasks = new List<Task>();
             foreach (var node in doc.DocumentNode.SelectNodes("//a[@class='channel-item__link']"))
             {
-                var task = LoadChannel(node.Attributes["href"].Value);
-                tasks.Add(task);
+                list_of_channels.Add(node.Attributes["href"].Value);
             }
-            await Task.WhenAll(tasks);
         }
 
-        static async Task LoadChannel(String channelName)
+        static async Task<UInt16> LoadChannel(String channelName)
         {
-            totalChannels++;
+            _totalChannels++;
             var urlToChannel = "";
             if (channelName.IndexOf("id/", StringComparison.Ordinal) >= 0)
             {
@@ -48,32 +61,61 @@ namespace ConsoleApp1
             {
                 urlToChannel = String.Format(api_channel_name, channelName.Substring(1));
             }
-            try
-            {
-                var request = await client.GetAsync(urlToChannel);
-                var text = await request.Content.ReadAsStringAsync();
-                var person = JsonSerializer.Deserialize<Person>(text);
-            }
-            catch
-            {
-                Console.WriteLine("Ошибка!!! Не удалось получить {0}", urlToChannel);
-            }
+            var text = await client.GetStringAsync(urlToChannel);
+            //var person = JsonSerializer.Deserialize<Person>(text); 
+            //var file = new FileStream($"{person.rid}.json", FileMode.Create, FileAccess.Write);
+            //file.Close();
+            return 1;
         }
         static async Task LoadPages()
         {
-            for (var i = 0; i < pages / pull_pages; i++)
+            List<Task<UInt16>> tasks = new List<Task<UInt16>>();
+            while (true)
             {
-                var tasks = new List<Task>();
-                for (var k = pull_pages*i+1; k <= pull_pages*(i+1); k++)
+                while (_pagesWorking < pull_pages && (_completedPages + _pagesWorking) < pages)
                 {
-                    var task = loader.LoadFromWebAsync($"https://zen.yandex.ru/media/zen/channels?page={k}");
-                    tasks.Add(Task.Run(() => task
-                        .ContinueWith(async t => await ParsePage(t.Result))
-                        .Unwrap()));
+                    var task = loader.LoadFromWebAsync($"https://zen.yandex.ru/media/zen/channels?page={_currentPage}")
+                        .ContinueWith((t) =>
+                        {
+                            ParsePage(t.Result);
+                            return (UInt16)2;
+                        });
+                    tasks.Add(task);
+                    _currentPage++;
+                    _pagesWorking++;
                 }
 
-                await Task.WhenAll(tasks);
-                Console.WriteLine("{1} Партия закончилась: {0} секунд", DateTime.Now - start, i+1);
+                while (list_of_channels.Count > 0 && _channelsWorking < pull_channels)
+                {
+                    var task = LoadChannel(list_of_channels[0]);
+                    list_of_channels.RemoveAt(0);
+                    tasks.Add(task);
+                    _channelsWorking++;
+                    _currentChannel++;
+                }
+
+                try
+                {
+                    var completedTask = await Task.WhenAny(tasks);
+                    var result = await completedTask;
+                    tasks.Remove(completedTask);
+                    switch (result)
+                    {
+                        case 1:
+                            _completedChannels++;
+                            _channelsWorking--;
+                            break;
+                        case 2:
+                            _completedPages++;
+                            _pagesWorking--;
+                            break;
+                    }
+                    GC.Collect();
+                }
+                catch
+                {
+                    break;
+                }
             }
         }
 
@@ -81,7 +123,7 @@ namespace ConsoleApp1
         {
             Console.WriteLine("Старт начинаем! {0}", start);
             await LoadPages();
-            Console.WriteLine("При {0} страницах было получено {1} каналов, среднее время на партию: {2}", pages, totalChannels, (DateTime.Now - start).TotalSeconds/pages);
+            Console.WriteLine("При {0} страницах было получено {1} каналов {2}", pages, _totalChannels, DateTime.Now - start);
         }
     }
 }
